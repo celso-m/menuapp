1bf76dff8919856bddf49e5b8d39e0e00a1c0470
celso <shinycelery@gmail.com>
2023-01-24 00:47:19 +0900

    adding state for chosen restaurant

--- src/components/Multiform.tsx
+++ src/components/Multiform.tsx
@@ -1,61 +1,68 @@
 import { useEffect, useState } from 'react';
 import dishes from '../dummydata/dishes.json'
 
-const formPages = [
-  {
-    name: 'Page 1',
-    inputs: [
-      { type: 'text', name: 'input1' },
-      {
-        type: 'dropdown',
-        name: 'meal',
-        options: ['breakfast', 'lunch', 'dinner'],
-        className: "mealDropDown"
-      },
-    ],
-  },
-  {
-    name: 'Page 2',
-    inputs: [
-      { type: 'text', name: 'input2' },
-      {
-        type: 'dropdown',
-        name: 'dropdown2',
-        options: ['Option 3', 'Option 4'],
-      },
-    ],
-  },
-  {
-    name: 'Page 3',
-    inputs: [
-      { type: 'text', name: 'input3' },
-      {
-        type: 'dropdown',
-        name: 'dropdown3',
-        options: ['Option 5', 'Option 6'],
-      },
-    ],
-  },
-  {
-    name: 'Page 4',
-    inputs: [
-      { type: 'text', name: 'input3' },
-      {
-        type: 'dropdown',
-        name: 'dropdown3',
-        options: ['Option 5', 'Option 6'],
-      },
-    ],
-  },
-];
 
 export default function Multiform() {
   const [currentPage, setCurrentPage] = useState(0);
   const [inputValues, setInputValues] = useState<{[key: string]: string}>({});
-  const [dishList, setDishes] = useState({})
+  const [dishList, setDishes] = useState([{}])
+  const selectableRest = Array.from(new Set(dishList
+    .filter((item:any) => item.availableMeals == inputValues['meal'])
+    .map((item:any) => item.restaurant ))
+  )
+
+  console.log(inputValues)
+  console.log(selectableRest)
+  const formPages = [
+    {
+      name: 'Page 1',
+      inputs: [
+        { type: 'text', name: 'input1' },
+        {
+          type: 'dropdown',
+          name: 'meal',
+          options: ['breakfast', 'lunch', 'dinner'],
+          className: "mealDropDown"
+        },
+      ],
+    },
+    {
+      name: 'Page 2',
+      inputs: [
+        { type: 'text', name: 'input2' },
+        {
+          type: 'dropdown',
+          name: 'dropdown2',
+          options: [...selectableRest],
+        },
+      ],
+    },
+    {
+      name: 'Page 3',
+      inputs: [
+        { type: 'text', name: 'input3' },
+        {
+          type: 'dropdown',
+          name: 'dropdown3',
+          options: ['Option 5', 'Option 6'],
+        },
+      ],
+    },
+    {
+      name: 'Page 4',
+      inputs: [
+        { type: 'text', name: 'input3' },
+        {
+          type: 'dropdown',
+          name: 'dropdown3',
+          options: ['Option 5', 'Option 6'],
+        },
+      ],
+    },
+  ];
 
   function dummyGetAPI(){
-    setDishes(dishes)
+    setDishes(dishes.dishes)
   } 
 
   useEffect(()=>{
@@ -105,17 +112,16 @@ export default function Multiform() {
   }
 
   const currentInputs = formPages[currentPage].inputs;
+  
   const inputMap = currentInputs.map((input:any, i) => (
     <div className={input.className} key={i}>
       {pageBuilder(input)}
-
     </div>
   ))
 
   return (
     <form>
       <div className="form">
-
       {inputMap}
       </div>
       {currentPage > 0 && <button onClick={handlePrev}>Previous</button>}
